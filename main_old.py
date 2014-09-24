ea108b25df8f6b7c29ed2b30fa1333922ef8a36b
Jorge <jorge@imatec.cl>
2014-09-24 15:21:38 -0300

    enhanced version

--- main_old.py
+++ main_old.py
@@ -4,10 +4,7 @@
 # SARCAN
 #
 ###########################################
-try:
-    import urllib.request as urllib2
-except:
-    import urllib2
+import urllib2
 import requests
 try:
     import json
@@ -15,7 +12,7 @@ except ImportError:
     import simplejson as json
 import os
 from base64 import *
-
+import glob
 def getRows(data):
     # ?? this totally depends on what's in your data
     return []
@@ -43,7 +40,7 @@ with open(basepath + f_fname, "w") as outfile:
 
 for f_item in f_data["Items"]:
 	FederationId = f_item["FederationId"]
-	print (FederationId)
+	print FederationId
 	# Create federation folder
 	federation_path = basepath + FederationId
 	if not os.path.exists(federation_path):
@@ -53,22 +50,25 @@ for f_item in f_data["Items"]:
 	documents_p_resource = "http://gosocketapi2.azurewebsites.net/api/App/GetSentDocuments?CountryId=cl&FederationId="+FederationId+"&AccountId=" + AccountId + "&Page=1"
 	dpr = requests.get(documents_p_resource, auth=(user, password))
 	dp_data = dpr.json()
+	
+	#pages = glob.glob("gosocket_downloads/" + FederationId + "/*.json")
+	pages = os.listdir("gosocket_downloads/" + FederationId + "")
+	#print pages[-1]
 
+	def getPageNums( filaname ):
+		splitted = filename.split("_")
+		return splitted[1].split("p")[1];
 
-	if os.path.isfile("gosocket_downloads/" + FederationId + "/lastpage"):	
-		f = open("gosocket_downloads/" + FederationId + "/lastpage", "r")
-		startingpage = int(f.readline())
-		f.close()
-	else:
-		startingpage = 1
+	pagenumbers = []
+	for filename in pages:
+		print getPageNums(filename)
+		pagenumbers.append(int(getPageNums(filename)))
+	pagenumbers.sort()
+	print pagenumbers
 
-	for page in range(startingpage, int(dp_data["TotalPages"])):
-		# Create page folder
-		page_path = federation_path + "/p" + str(page) 
-		if not os.path.exists(page_path):
-			os.makedirs(page_path)
+	for page in range(pagenumbers[-1], int(dp_data["TotalPages"])):
 
-		print ("iterating page " + str(page))
+		print "iterating page " + str(page)
 		# Iterate over documents of a federation
 		documents_resource = "http://gosocketapi2.azurewebsites.net/api/App/GetSentDocuments?CountryId=cl&FederationId="+FederationId+"&AccountId=" + AccountId + "&Page=" + str(page)
 		dr = requests.get(documents_resource, auth=(user, password))
@@ -78,37 +78,33 @@ for f_item in f_data["Items"]:
 		d_fname = "federation_p" + str(page) + "_" + FederationId + ".json"	
 		with open(federation_path + "/" + d_fname, "w") as outfile:
 			json.dump(d_data, outfile)
+		
+		# for d_item in d_data["Items"]:
+		# 	DocumentId = d_item["DocumentId"]
+		# 	print " > " + DocumentId
 
-		for d_item in d_data["Items"]:
-			DocumentId = d_item["DocumentId"]
-			print (" > " + DocumentId)
+		# 	# Create document folder
+		# 	document_path = federation_path + "/" + DocumentId
+		# 	if not os.path.exists(document_path):
+		# 		os.makedirs(document_path)
 
-			# Create document folder
-			document_path = page_path + "/" + DocumentId
-			if not os.path.exists(document_path):
-				os.makedirs(document_path)
+		# 	# Iterate over details of a document
+		# 	documentDetail_resource = "http://gosocketapi2.azurewebsites.net/api/App/GetDocumentDetail?CountryId=cl&DocumentId=" + DocumentId
+		# 	dDr = requests.get(documentDetail_resource, auth=(user, password))
+		# 	dD_data = dDr.json()
+		# 	# Copy document detail JSON
+		# 	dD_fname = "documentDetail_" + DocumentId + ".json"	
+		# 	with open(document_path + "/" + dD_fname, "w") as outfile:
+		# 		json.dump(dD_data, outfile)
 
-			# Iterate over details of a document
-			documentDetail_resource = "http://gosocketapi2.azurewebsites.net/api/App/GetDocumentDetail?CountryId=cl&DocumentId=" + DocumentId
-			dDr = requests.get(documentDetail_resource, auth=(user, password))
-			dD_data = dDr.json()
-			# Copy document detail JSON
-			dD_fname = "documentDetail_" + DocumentId + ".json"	
-			with open(document_path + "/" + dD_fname, "w") as outfile:
-				json.dump(dD_data, outfile)
+		# 	# Copy document detail XML
+		# 	url = "http://api.gosocket.net/api/App/GetXml?CountryId=cl&DocumentId=b258b4b1-52ff-43dd-8a7c-0e023852b0f8"
 
-			# Copy document detail XML
-			url = "http://api.gosocket.net/api/App/GetXml?CountryId=cl&DocumentId=" + DocumentId
+		# 	xml_r = requests.get(url, auth=(user, password))
+		# 	# print r.text
+		# 	xml_data = xml_r.text
+		# 	xml_fname = "xml_" + DocumentId + ".xml"	
 
-			xml_r = requests.get(url, auth=(user, password))
-		    
-			xml_data = xml_r.text
-			xml_test = str(b64decode(xml_data).decode())
-			xml_fname = "xml_" + DocumentId + ".xml"
-
-			xml_file = open(document_path + "/" + xml_fname, "w")
-			xml_file.write(xml_test)
-			xml_file.close()
-		f2 = open("gosocket_downloads/" + FederationId + "/lastpage", "w")
-		f2.write(str(page))
-		f2.close
+		# 	xml_file = open(document_path + "/" + xml_fname, "w")
+		# 	xml_file.write(b64decode(xml_data))
+		# 	xml_file.close()
